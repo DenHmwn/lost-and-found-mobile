194c94af36a452a692814d3742508c812be284b4
Deni Himawan <denihimawan595@gmail.com>
2025-11-18 00:29:02 +0700

    validasi id di dell pada found

--- api/src/app/api/foundreport/[slug]/route.ts
+++ api/src/app/api/foundreport/[slug]/route.ts
@@ -266,4 +266,15 @@ export async function DELETE(
   { params }: { params: Promise<{ slug: string }> }
 ) {
   const { slug } = await params;
+  // Validasi ID
+    const id = Number(slug);
+    if (isNaN(id)) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "ID tidak valid",
+        },
+        { status: 400 }
+      );
+    }
 }
