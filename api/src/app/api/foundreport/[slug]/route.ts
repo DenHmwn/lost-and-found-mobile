01222328511074dbf48a81c060685c15bc9fec48
Deni Himawan <denihimawan595@gmail.com>
2025-11-17 23:01:30 +0700

    buat validasi id

--- api/src/app/api/foundreport/[slug]/route.ts
+++ api/src/app/api/foundreport/[slug]/route.ts
@@ -1,5 +1,20 @@
+import { NextResponse } from "next/server";
+
 // buat Fungsi GET
 export async function GET(
   request: Request,
   { params }: { params: Promise<{ slug: string }> }
-) {}
+) {const { slug } = await params;
+
+    // Validasi ID
+    const id = Number(slug);
+    if (isNaN(id)) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "ID tidak valid",
+        },
+        { status: 400 }
+      );
+    }
+}
