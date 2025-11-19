544835352a51f285a98c53153381497b81eccd39
Deni Himawan <denihimawan595@gmail.com>
2025-11-19 14:41:58 +0700

    buat fungsi Delete lost

--- api/src/app/api/lostreport/[slug]/route.ts
+++ api/src/app/api/lostreport/[slug]/route.ts
@@ -197,3 +197,23 @@ export async function PUT(
     );
   }
 }
+
+// Delete LostReport
+export async function DELETE(
+  request: Request,
+  { params }: { params: Promise<{ slug: string }> }
+) {
+    const { slug } = await params;
+
+    // Validasi ID
+    const id = Number(slug);
+    if (isNaN(id)) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "ID tidak valid",
+        },
+        { status: 400 }
+      );
+    }
+  }
