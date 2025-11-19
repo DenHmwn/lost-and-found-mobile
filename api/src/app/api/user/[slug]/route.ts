03bd3c64373dc32bda1b75e84f02b8feca94461f
Deni Himawan <denihimawan595@gmail.com>
2025-11-19 15:01:28 +0700

    buat fungsi GET user by id dan vlidasi id

--- api/src/app/api/user/[slug]/route.ts
+++ api/src/app/api/user/[slug]/route.ts
@@ -106,3 +106,23 @@ export const PUT = async (
     });
   }
 };
+
+// GET User by id
+export async function GET(
+  request: Request,
+  { params }: { params: Promise<{ slug: string }> }
+) {
+    const { slug } = await params;
+
+    // Validasi ID
+    const id = Number(slug);
+    if (isNaN(id)) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "ID tidak valid",
+        },
+        { status: 400 }
+      );
+    }
+}
