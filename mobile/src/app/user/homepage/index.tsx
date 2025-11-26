19d144067d7c48c166f0916a7c56723095ad76cb
Deni Himawan <denihimawan595@gmail.com>
2025-11-26 18:25:35 +0700

    misahin styles header

--- mobile/src/app/user/homepage/index.tsx
+++ mobile/src/app/user/homepage/index.tsx
@@ -1,19 +1,22 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import { BottomNavigation, PaperProvider } from 'react-native-paper';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-
-import LostItemPage from '../lostitems';
-import FoundItemPage from '../founditems';
-import AccountPageUser from '../account';
+import React, { useState } from "react";
+import { View, Text } from "react-native";
+import { Appbar, BottomNavigation, PaperProvider } from "react-native-paper";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { styles } from "@/style/styles";
+import LostItemPage from "../lostitems";
+import FoundItemPage from "../founditems";
+import AccountPageUser from "../account";
 
 // Halaman HomePage
 const HomeRoute = () => (
-  <View style={{ flex: 1, justifyContent: "flex-start" }}>
-    <Text style={[styles.warna_bg, { fontSize: 25, textAlign: "center" }]}>
-      Lost and Found
-    </Text>
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+  <View>
+    <Appbar.Header style={styles.header}>
+      {/* <Appbar.BackAction onPress={_goBack} /> */}
+      <Appbar.Content title="Lost & Found" style={{alignItems: "center"}} />
+      {/* <Appbar.Action icon="magnify" onPress={_handleSearch} /> */}
+      {/* <Appbar.Action icon="dots-vertical" onPress={_handleMore} /> */}
+    </Appbar.Header>
+    <View style={{justifyContent: "center", alignItems: "center",}}>
       <Text>Halaman Home</Text>
     </View>
   </View>
@@ -22,10 +25,30 @@ const HomeRoute = () => (
 export default function HomePageUser() {
   const [index, setIndex] = useState(0);
   const [routes] = useState([
-    { key: 'home', title: 'Home', focusedIcon: 'home', unfocusedIcon: 'home-outline' },
-    { key: 'lost', title: 'lost', focusedIcon: 'help-box', unfocusedIcon: 'box-variant' },
-    { key: 'found', title: 'found', focusedIcon: 'briefcase-check', unfocusedIcon: 'briefcase-check-outline' },
-    { key: 'account', title: 'Account', focusedIcon: 'account', unfocusedIcon: 'account-outline' },
+    {
+      key: "home",
+      title: "Home",
+      focusedIcon: "home",
+      unfocusedIcon: "home-outline",
+    },
+    {
+      key: "lost",
+      title: "lost",
+      focusedIcon: "help-box",
+      unfocusedIcon: "box-variant",
+    },
+    {
+      key: "found",
+      title: "found",
+      focusedIcon: "briefcase-check",
+      unfocusedIcon: "briefcase-check-outline",
+    },
+    {
+      key: "account",
+      title: "Account",
+      focusedIcon: "account",
+      unfocusedIcon: "account-outline",
+    },
   ]);
 
   const renderScene = BottomNavigation.SceneMap({
@@ -42,20 +65,9 @@ export default function HomePageUser() {
           navigationState={{ index, routes }}
           onIndexChange={setIndex}
           renderScene={renderScene}
-          barStyle={{ backgroundColor: '#486feeff' }} 
+          barStyle={{ backgroundColor: "#486feeff" }}
         />
       </PaperProvider>
     </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  warna_bg: {
-    backgroundColor: "gray",
-    paddingBottom: 10,
-    paddingTop: 20,
-    paddingHorizontal: 20,
-    color: 'white',
-    fontWeight: 'bold'
-  },
-});
--- mobile/src/app/user/lostitems/index.tsx
+++ mobile/src/app/user/lostitems/index.tsx
@@ -1,25 +1,21 @@
 // File: LostItemPage.tsx
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { styles } from "@/style/styles";
+import React from "react";
+import { View, Text } from "react-native";
+import { Appbar } from "react-native-paper";
 
 export default function LostItemPage() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Ini adalah Halaman Barang Hilang</Text>
+    <View>
+      <Appbar.Header style={styles.header}>
+        {/* <Appbar.BackAction onPress={_goBack} /> */}
+        <Appbar.Content title="Lost & Found" style={{ alignItems: "center" }} />
+        {/* <Appbar.Action icon="magnify" onPress={_handleSearch} /> */}
+        {/* <Appbar.Action icon="dots-vertical" onPress={_handleMore} /> */}
+      </Appbar.Header>
+      <View style={{ justifyContent: "center", alignItems: "center" }}>
+        <Text>Halaman List Kehilangan Barang</Text>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    // backgroundColor: 'gray',
-  },
-  text: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginBottom: 10,
-  },
-});
