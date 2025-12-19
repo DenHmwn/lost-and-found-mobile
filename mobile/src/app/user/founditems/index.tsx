9bdbdf5e5e08e3efb7fef98831bb102f8babf790
Deni Himawan <denihimawan595@gmail.com>
2025-12-20 00:44:39 +0700

    feat: update list found

--- mobile/src/app/user/founditems/index.tsx
+++ mobile/src/app/user/founditems/index.tsx
@@ -5,6 +5,39 @@ import { View, Text } from "react-native";
 import { Appbar } from "react-native-paper";
 
 export default function FoundItemPage() {
+  const [ListLost, setListLost] = useState<FoundReport[]>([]);
+
+  useEffect(() => {
+    getBarangLost();
+  }, []);
+
+  // get data dari API
+  const getBarangLost = async () => {
+    try {
+      const response = await axios.get(strings.api_found);
+
+      // Cek apa isi response di Terminal Metro Bundler
+      // console.log("DATA DARI API:", JSON.stringify(response.data, null, 2));
+
+      // Cek struktur data sebelum set state
+      if (Array.isArray(response.data)) {
+        setListLost(response.data);
+      } else if (response.data.data) {
+        setListLost(response.data.data);
+      } else {
+        // console.log("Struktur data tidak dikenali, cek backend");
+      }
+    } catch (error) {
+      console.error("Error ambil data:", error);
+    }
+  };
+
+  const [visible, setVisible] = React.useState(false);
+  const showDialog = () => setVisible(true);
+  const hideDialog = () => setVisible(false);
+  const message = useRef("");
+  const [id, setId] = useState(0);
+
   return (
     <View style={{ flex: 1, justifyContent: "flex-start" }}>
       <Appbar.Header style={styles.background}>
