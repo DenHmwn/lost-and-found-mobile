93d5e34420c930476f1c763012d134192b88df37
Deni Himawan <denihimawan595@gmail.com>
2025-12-20 00:44:28 +0700

    feat: update list found

--- mobile/src/app/user/founditems/index.tsx
+++ mobile/src/app/user/founditems/index.tsx
@@ -18,6 +18,29 @@ export default function FoundItemPage() {
       <View style={styles.pageTitleContainer}>
         <Text style={styles.PageTitle}>Halaman List Penemuan Barang</Text>
       </View>
+      <FlatList
+        style={{ flex: 1 }}
+        contentContainerStyle={{ paddingBottom: 50 }}
+        data={ListLost}
+        keyExtractor={(item) => String(item.id)}
+        renderItem={({ item }) => (
+          <Card key={item.id} style={styles.card}>
+            <Card.Title
+              title={item.namaBarang}
+              subtitle={item.lokasiTemu}
+              titleStyle={{ fontSize: 20 }}
+            />
+            <Card.Actions>
+              <Button
+                onPress={() => console.log("edit")}
+                style={{ backgroundColor: "#5B7FFF" }}
+              >
+                <MaterialIcons name="info" size={24} color="black" />
+              </Button>
+            </Card.Actions>
+          </Card>
+        )}
+      />
     </View>
   );
 }
