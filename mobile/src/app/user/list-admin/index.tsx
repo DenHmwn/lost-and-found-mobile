c64751d90dc3dcf60a2b37f5f4d1e7c5c9d6c869
Deni Himawan <denihimawan595@gmail.com>
2025-12-19 20:43:17 +0700

    feat: import component

--- mobile/src/app/user/list-admin/index.tsx
+++ mobile/src/app/user/list-admin/index.tsx
@@ -1,5 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { Appbar } from 'react-native-paper'
+import { styles } from '@/style/styles'
 
 export default function ListAdminPage() {
   return (
