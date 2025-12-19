c0194ef688cbde125849d35b6bfb6ff70cd0ece3
Deni Himawan <denihimawan595@gmail.com>
2025-12-19 20:45:42 +0700

    feat: update page account

--- mobile/src/app/user/account/index.tsx
+++ mobile/src/app/user/account/index.tsx
@@ -4,8 +4,14 @@ import { View, Text, StyleSheet } from 'react-native';
 
 export default function AccountPageUser() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Ini adalah Halaman Account User</Text>
+    <View style={{ flex: 1, justifyContent: "flex-start" }}>
+      <Appbar.Header style={styles.background}>
+        <Appbar.Content
+          title="Lost & Found"
+          titleStyle={styles.PageTitle}
+          style={styles.PageTitle}
+        />
+      </Appbar.Header>
     </View>
   );
 }
