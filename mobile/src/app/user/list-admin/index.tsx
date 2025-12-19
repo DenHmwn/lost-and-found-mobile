6df03fbd50ec2889d062d81637d5ecbee584f391
Deni Himawan <denihimawan595@gmail.com>
2025-12-19 20:43:05 +0700

    feat: update header list admin

--- mobile/src/app/user/list-admin/index.tsx
+++ mobile/src/app/user/list-admin/index.tsx
@@ -3,8 +3,18 @@ import React from 'react'
 
 export default function ListAdminPage() {
   return (
-    <View>
-      <Text>index</Text>
+   <View style={{ flex: 1, justifyContent: "flex-start" }}>
+      <Appbar.Header style={styles.background}>
+        <Appbar.Content
+          title="Lost & Found"
+          titleStyle={styles.PageTitle}
+          style={styles.PageTitle}
+        />
+      </Appbar.Header>
+
+      <View style={styles.pageTitleContainer}>
+        <Text style={styles.PageTitle}>Halaman List Admin</Text>
+      </View>
     </View>
   )
 }
