47d1e6c25194a4a2cba0731a0ee3c8bac2eca429
Dika Ramadhani <122988951+DikaRamadhani@users.noreply.github.com>
2026-01-02 22:06:19 +0700

    chore(dashboard): import StatCard

--- mobile/src/app/admin/dashboard/index.tsx
+++ mobile/src/app/admin/dashboard/index.tsx
@@ -1,2 +1,3 @@
 import React, { useEffect, useState } from "react";
 import { ActivityIndicator, RefreshControl, ScrollView, StyleSheet, Text, View } from "react-native";
+import StatCard from "@/components/StatCard";
