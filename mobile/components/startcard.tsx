40b93b2fc5b963b496a51e63b09d97ee5b60a167
Dika Ramadhani <122988951+DikaRamadhani@users.noreply.github.com>
2026-01-02 22:41:02 +0700

    feat(stat-card): add StatCard component

--- mobile/components/startcard.tsx
+++ mobile/components/startcard.tsx
@@ -1,2 +1,11 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+
+export default function StatCard({ label, value }: { label: string; value: number }) {
+  return (
+    <View>
+      <Text>{label}</Text>
+      <Text>{value}</Text>
+    </View>
+  );
+}
