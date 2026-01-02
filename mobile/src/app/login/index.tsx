85bac9be17595b5e256ecf2ee9c79d7e1af7a523
Dika Ramadhani <122988951+DikaRamadhani@users.noreply.github.com>
2026-01-02 21:23:04 +0700

    chore(login): add LoginPage component

--- mobile/src/app/login/index.tsx
+++ mobile/src/app/login/index.tsx
@@ -1,3 +1,6 @@
 import React from 'react';
 import { StyleSheet, View, Text, TouchableOpacity, StatusBar } from 'react-native';
 import { router } from 'expo-router';
+
+export default function LoginPage() {   
+}
