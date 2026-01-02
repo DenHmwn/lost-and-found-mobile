b34f233ce93f9390d7c430c55f01fb4002d40006
Dika Ramadhani <122988951+DikaRamadhani@users.noreply.github.com>
2026-01-02 22:31:13 +0700

    style(badge): apply styles and dynamic background

--- mobile/components/badge.tsx
+++ mobile/components/badge.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import { StyleSheet, Text } from "react-native";
 
 export default function Badge({ text, bg }: { text: string; bg: string }) {
-  return <Text style={{ backgroundColor: bg }}>{text}</Text>;
+  return <Text style={[styles.badge, { backgroundColor: bg }]}>{text}</Text>;
+
 }
 
 const styles = StyleSheet.create({
