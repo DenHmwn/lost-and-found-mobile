67eb0ed5baffc07476255fceb2bdde0a04f8a03b
Dika Ramadhani <122988951+DikaRamadhani@users.noreply.github.com>
2026-01-02 22:29:26 +0700

    feat(badge): add Badge component

--- mobile/components/badge.tsx
+++ mobile/components/badge.tsx
@@ -1,2 +1,6 @@
 import React from "react";
 import { StyleSheet, Text } from "react-native";
+
+export default function Badge({ text, bg }: { text: string; bg: string }) {
+  return <Text style={{ backgroundColor: bg }}>{text}</Text>;
+}
