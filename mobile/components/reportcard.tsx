de57949ff43c4039752b1dc20b343a6af2c3b04c
Dika Ramadhani <122988951+DikaRamadhani@users.noreply.github.com>
2026-01-02 22:35:50 +0700

    chore(report-card): import formatDateTime

--- mobile/components/reportcard.tsx
+++ mobile/components/reportcard.tsx
@@ -2,3 +2,4 @@ import React from "react";
 import { Pressable, StyleSheet, Text, View } from "react-native";
 import Badge from "./Badge";
 import { UiReport } from "../types/admin.ui";
+import { formatDateTime } from "../utils/date";
