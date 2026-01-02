928d8393739c85ed4dd97c2ebc847b064484407f
Dika Ramadhani <122988951+DikaRamadhani@users.noreply.github.com>
2026-01-02 21:51:18 +0700

    feat(dashboard-stack): register dashboard index screen

--- mobile/src/app/admin/dashboard/_layout.tsx
+++ mobile/src/app/admin/dashboard/_layout.tsx
@@ -5,6 +5,7 @@ import { Stack } from "expo-router";
 export default function DashboardStack() {
     return (
     <Stack screenOptions={{ headerTitleStyle: { fontWeight: "900" } }}>
+        <Stack.Screen name="index" options={{ title: "Dashboard" }} />
     </Stack>
   );
 
