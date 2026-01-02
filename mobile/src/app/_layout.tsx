3912426926392d3e7d4951a3a9cd472cc68f301e
Dika Ramadhani <122988951+DikaRamadhani@users.noreply.github.com>
2026-01-02 20:06:08 +0700

    feat(layout): register index screen

--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -4,7 +4,8 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
     return (
     <Stack screenOptions={{ headerShown: false }}>
-        {/* screens */}
+        <Stack.Screen name="index" />
+
     </Stack>
 
      );
